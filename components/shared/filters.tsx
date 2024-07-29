e77f17621723f8b6a915f6c4e4c2baf85f8e043d
Slipum <123569528+Slipum@users.noreply.github.com>
2024-07-29 20:47:32 +0300

    update filters

--- components/shared/filters.tsx
+++ components/shared/filters.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Input } from '../ui';
 import { FilterCheckbox } from './filter-checkbox';
+import { RangeSlider } from './range-slider';
 import { Title } from './title';
 
 interface Props {
@@ -17,6 +19,12 @@ export const Filters: React.FC<Props> = ({ className }) => {
 
 			<div className="mt-5 border-y border-y-neutral-100 py-6 pb-7">
 				<p className="font-bold mb-3">Price</p>
+				<div className="flex gap-3 mb-5">
+					<Input type="number" placeholder="0" min={0} max={2000} defaultValue={0} />
+					<Input type="number" min={1} max={2000} placeholder="2000" />
+				</div>
+
+				<RangeSlider min={0} max={2000} step={2} value={[0, 2000]} />
 			</div>
 		</div>
 	);
--- components/shared/index.ts
+++ components/shared/index.ts
@@ -3,6 +3,7 @@ export { Container } from './container';
 export { FilterCheckbox } from './filter-checkbox';
 export { Filters } from './filters';
 export { Header } from './header';
+export { RangeSlider } from './range-slider';
 export { SortPopup } from './sort-popup';
 export { Title } from './title';
 export { TopBar } from './top-bar';
