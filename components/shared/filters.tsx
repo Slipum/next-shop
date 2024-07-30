772f7f4d64183f110ef23fd4f3f8853e5565109e
Slipum <123569528+Slipum@users.noreply.github.com>
2024-07-30 23:29:00 +0300

    update filters

--- components/shared/filters.tsx
+++ components/shared/filters.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Input } from '../ui';
+import { CheckboxFiltersGroup } from './checkbox-filters-group';
 import { FilterCheckbox } from './filter-checkbox';
 import { RangeSlider } from './range-slider';
 import { Title } from './title';
@@ -13,8 +14,9 @@ export const Filters: React.FC<Props> = ({ className }) => {
 		<div className={className}>
 			<Title text="Filters" size="sm" className="mb-5 font-bold" />
 			<div className="flex flex-col gap-4">
-				<FilterCheckbox text="anyware" value="1" />
-				<FilterCheckbox text="something" value="2" />
+				<FilterCheckbox text="New" value="1" />
+				<FilterCheckbox text="Renewed" value="2" />
+				<FilterCheckbox text="Used" value="3" />
 			</div>
 
 			<div className="mt-5 border-y border-y-neutral-100 py-6 pb-7">
@@ -26,6 +28,79 @@ export const Filters: React.FC<Props> = ({ className }) => {
 
 				<RangeSlider min={0} max={2000} step={2} value={[0, 2000]} />
 			</div>
+
+			<CheckboxFiltersGroup
+				title="Laptop Display Size"
+				limit={6}
+				defaultItems={[
+					{
+						text: '17 Inches & Above',
+						value: '1',
+					},
+					{
+						text: '16 to 16.9 Inches',
+						value: '2',
+					},
+					{
+						text: '15 to 15.9 Inches',
+						value: '3',
+					},
+					{
+						text: '14 to 14.9 Inches',
+						value: '4',
+					},
+					{
+						text: '13 to 13.9 Inches',
+						value: '5',
+					},
+					{
+						text: '12 to 12.9 Inches',
+						value: '6',
+					},
+					{
+						text: '11 to 11.9 Inches',
+						value: '7',
+					},
+					{
+						text: '11 Inches & Under',
+						value: '8',
+					},
+				]}
+				items={[
+					{
+						text: '17 Inches & Above',
+						value: '1',
+					},
+					{
+						text: '16 to 16.9 Inches',
+						value: '2',
+					},
+					{
+						text: '15 to 15.9 Inches',
+						value: '3',
+					},
+					{
+						text: '14 to 14.9 Inches',
+						value: '4',
+					},
+					{
+						text: '13 to 13.9 Inches',
+						value: '5',
+					},
+					{
+						text: '12 to 12.9 Inches',
+						value: '6',
+					},
+					{
+						text: '11 to 11.9 Inches',
+						value: '7',
+					},
+					{
+						text: '11 Inches & Under',
+						value: '8',
+					},
+				]}
+			/>
 		</div>
 	);
 };
