f33d940aeee167688c040cf1ca3901ce9c9fc0f2
Slipum <123569528+Slipum@users.noreply.github.com>
2024-08-01 20:01:35 +0300

    update models | add seeding prisma

---
+++ prisma/constants.ts
@@ -0,0 +1,7 @@
+export const categories = [
+	{ name: 'Laptops' },
+	{ name: 'Phones' },
+	{ name: 'Tablets' },
+	{ name: 'Servers' },
+	{ name: 'Keyboards' },
+];
---
+++ prisma/seed.ts
@@ -0,0 +1,51 @@
+import { hashSync } from 'bcrypt';
+import { categories } from './constants';
+import { prisma } from './prisma-client';
+
+async function up() {
+	await prisma.user.createMany({
+		data: [
+			{
+				fullName: 'User Test',
+				email: 'user@test.com',
+				password: hashSync('11111111', 10),
+				verified: new Date(),
+				role: 'USER',
+			},
+			{
+				fullName: 'Admin Admin',
+				email: 'admin@test.com',
+				password: hashSync('11111111', 10),
+				verified: new Date(),
+				role: 'ADMIN',
+			},
+		],
+	});
+
+	await prisma.category.createMany({
+		data: categories,
+	});
+}
+
+async function down() {
+	await prisma.$executeRaw`TRUNCATE TABLE "User" RESTART IDENTITY CASCADE`;
+}
+
+async function main() {
+	try {
+		await down();
+		await up();
+	} catch (e) {
+		console.error(e);
+	}
+}
+
+main()
+	.then(async () => {
+		await prisma.$disconnect();
+	})
+	.catch(async (e) => {
+		console.error(e);
+		await prisma.$disconnect();
+		process.exit(1);
+	});
